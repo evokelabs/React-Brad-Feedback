8e77e0ddd5368a4ad6abcc5caf6356ca38aee2f8
Adrian Pikios <adrian@evokelabs.com>
2022-02-18 14:30:00 +1100

    Syntax Update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
-function App() {    
-    return <h1>Hello from the app component</h1>    
+function App() {
+  const title = "Blog Post"
+  const body = "This is my blog post"
+  const comments = [
+    { id: 1, text: "Comment one" },
+    { id: 2, text: "Comment two" },
+    { id: 3, text: "Comment three" },
+  ]
+
+  const loading = false
+  const showComments = true
+
+  const commentBlock = (<div className="comments">
+  <h3>Comments ({comments.length})</h3>
+  <ul>
+    {comments.map((comment, index) => (
+      <li key={index}>{comment.text}</li>
+    ))}
+  </ul>
+</div>)
+
+  if (loading) return <h1>Loading...</h1>
+
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{body}</p>
+      {showComments && (commentBlock)}
+    </div>
+  )
 }
 
 export default App
